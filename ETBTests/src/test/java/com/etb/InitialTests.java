9b092ebb0e9526689fdb37a2812bc5d171e237b4
Paul Herron <paul.heron@nln.ie>
2019-11-11 10:35:28 +0000

    Initial Commit

---
+++ ETBTests/src/test/java/com/etb/InitialTests.java
@@ -0,0 +1,15 @@
+package com.etb;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InitialTests {
+
+	@Test
+	public void test() {
+		Pages.homePage().goTo();
+		assertTrue(Pages.homePage().isAt());
+	}
+
+}
